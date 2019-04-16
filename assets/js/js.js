d0b8b4fddc4caf6ec87d6697da4cca633135b9c5
Calvin Shin <calvin.shin@gmail.com>
2019-04-15 20:31:14 -0700

    Removed sample data, improved the bootstrap elements

--- assets/js/js.js
+++ assets/js/js.js
@@ -5,7 +5,7 @@ var app = {
     initialize : function() {
         this.searchterm = "cats"
     },
-
+ 
     // Functions
     imagepull : function() {
         $.ajax({
@@ -13,26 +13,29 @@ var app = {
             method: "GET"
         }).then( function(response) {
             console.log(response);
+            var data = response.data;
 
-            var results = response.data;
+            for (var image = 0; image < data.length; image++) {
+                var containerdiv = $("<div>");
+                containerdiv.addClass("card border-secondary");
 
-            for (var i = 0; i < results.length; i++) {
-              var gifDiv = $("<div>");
-  
-              var rating = results[i].rating;
-  
-              var p = $("<p>").text("Rating: " + rating);
-  
-              var personImage = $("<img>");
-              personImage.attr("src", results[i].images.fixed_height.url);
-  
-              gifDiv.prepend(p);
-              gifDiv.prepend(personImage);
-  
-              $("#randompulldiv").prepend(gifDiv);
+                var img = $("<img>");
+                img.addClass("card-img-top p-2 border-secondary");
+                img.attr("src", data[image].images.fixed_height.url)
+
+                var cardbody = $("<div>");
+                cardbody.addClass("card-body")
+
+                    var title = $("<h5>");
+                    title.addClass("card-title");
+                    title.html(data[image].title);
+
+                    cardbody.append(title);
+
+                containerdiv.append(img, cardbody);
+
+                $("#randompulldiv").prepend(containerdiv)
             }
-
-            // 
         })
     }
 }
@@ -41,3 +44,12 @@ app.initialize();
 app.imagepull();
 
 //  "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=10"
+
+
+// <div class="card border-secondary">
+//     <img class="card-img-top p-2 border-secondary" src="C:\Users\Calvin2\Desktop\Pixels\fantasyenemycreatures_windows\FantasyEnemyCreatures\Minotaur\Gifs\128x80Minotaur_Attack.gif" alt="Pokemon Hangman">
+//     <div class="card-body">
+//         <h5 class="card-title">Pokemon Hangman</h5>
+//         <p class="card-text">Pokemon hangman game using vanilla js and bootstrap. Uses functional programming.</p>
+//     </div>
+// </div>
