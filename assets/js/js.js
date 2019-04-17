083952f23ac378e773268502a8c5a3cf16f57003
Calvin Shin <calvin.shin@gmail.com>
2019-04-16 22:01:54 -0700

    Using the wrong API, this one calls on the top 10 images rather than random. Can potentiall yfix by updating an offset for each item

--- assets/js/js.js
+++ assets/js/js.js
@@ -3,45 +3,121 @@
 var app = {
     // ajax variables that are important
     initialize : function() {
-        this.searchterm = "cats"
+        this.searchterm = "cats";
+        this.animalArray = ["cats", "dogs", "rabbits", "mongooses", "koalas", "manatees", "narwhals", "reindeer"];
+        this.numberResults = 0;
+        this.buttonCreation(this.animalArray);  
     },
- 
+
     // Functions
+    buttonCreation : function(array) {
+        // Create buttons for the values in the searchArray.
+        for(var i = 0; i < array.length ; i++) {
+            var buttondiv = $("<button>");
+            buttondiv.addClass("btn btn-dark mx-2 my-1 button");
+            buttondiv.text(array[i]);
+
+            $("#buttoncontainer").append(buttondiv);
+        }
+
+        this.buttonListener();
+    },
+
     imagepull : function() {
         $.ajax({
-            url: "https://api.giphy.com/v1/gifs/search?q=" + this.searchterm + "&api_key=dc6zaTOxFJmzC&limit=10",
+            url: "https://api.giphy.com/v1/gifs/search?q=" + 
+                this.searchterm + "&api_key=dc6zaTOxFJmzC&limit=10",
             method: "GET"
         }).then( function(response) {
             console.log(response);
             var data = response.data;
 
             for (var image = 0; image < data.length; image++) {
+                // container div
                 var containerdiv = $("<div>");
-                containerdiv.addClass("card border-secondary");
+                containerdiv.addClass("card border-secondary cardcontainer");
 
+                // image div
                 var img = $("<img>");
-                img.addClass("card-img-top p-2 border-secondary");
-                img.attr("src", data[image].images.fixed_height.url)
+                img.addClass("card-img-top p-2 border-secondary image");
+                img.attr("src", data[image].images.fixed_height_still.url)
+                img.attr("state", "still")
+                img.attr("animated", data[image].images.fixed_height.url)
+                img.attr("still", data[image].images.fixed_height_still.url)
 
                 var cardbody = $("<div>");
                 cardbody.addClass("card-body")
 
-                    var title = $("<h5>");
-                    title.addClass("card-title");
-                    title.html(data[image].title);
+                var title = $("<h5>");
+                title.addClass("card-title");
+                title.html(data[image].title);
 
-                    cardbody.append(title);
-
+                cardbody.append(title);
                 containerdiv.append(img, cardbody);
 
-                $("#randompulldiv").prepend(containerdiv)
+                // problem: cards due things top down, then left right. Images should be inserted so all 10 images are viewable immediately.
+                if(app.numberResults === 0) {
+                    $("#randompulldiv").prepend(containerdiv)
+                }
+                // else if(image === 0) {
+                //     $("#randompulldiv").prepend(containerdiv);
+                //     console.log("test0");
+                // }
+                else if(image <= 3) {
+                    console.log("first 3")
+                    // var insertfirst = Math.floor(app.numberResults / 3) * 2 + 1
+                    var insertfirst = app.numberResults - 3 * app.numberResults / 10 + 1;
+                    console.log(insertfirst);
+                    containerdiv.insertBefore($(".cardcontainer:nth-child(" + insertfirst + ")"));
+                }
+                else if(image <= 6) {
+                    console.log(Math.floor(app.numberResults / 3) * 2 + 1);
+                    console.log("second 3")
+                    var insertsecond = app.numberResults - 6 * app.numberResults / 10 + 1;
+                    console.log(insertsecond);
+                    containerdiv.insertBefore($(".cardcontainer:nth-child(" + insertsecond + ")"));
+                }
+                else{
+                    console.log($(".cardcontainer:nth-child(1)"));
+                    console.log("last 3")
+                    containerdiv.insertBefore($(".cardcontainer:nth-child(" + 1 + ")"));
+                }
+
+                // $("#randompulldiv").prepend(containerdiv)
             }
+            app.gifListener();
+            app.numberResults += 10;
         })
+    },
+
+    buttonListener : function() {
+        app.buttonListenerVariable = $(".button").on("click", function() {
+
+            app.searchterm = $(this).text();
+            app.imagepull()
+        })
+    },
+
+    gifListener : function() {
+        // console.log("confirmed")
+        app.gifListenerVariable = $(".image").on("click", function() {
+            var state = $(this).attr("state");
+            if(state === "still") {
+                $(this).attr("src", $(this).attr("animated"))
+                $(this).attr("state", "animated");
+            }
+            else {
+                $(this).attr("src", $(this).attr("still"))
+                $(this).attr("state", "still");
+            }
+        });
     }
 }
 
 app.initialize();
 app.imagepull();
+
+
 
 //  "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=10"
 
