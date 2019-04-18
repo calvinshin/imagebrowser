0e85bcb434972a005fcc595a0d56a36574c585ae
Calvin Shin <calvin.shin@gmail.com>
2019-04-17 21:47:07 -0700

    Created a favorites bar that allows a user to save images

--- assets/js/js.js
+++ assets/js/js.js
@@ -13,22 +13,28 @@ var app = {
         this.inputListener();
     },
 
+    // triggered on the input being pressed
     inputFunction : function() {
-        // Update the input as a value; set it as a local variable
-        var input = $("input").val();
-        // clear the input on the screen
-        $("input").val("");
+        if($("input").val().trim() != "") {
+            // Update the input as a value; set it as a local variable
+            var input = $("input").val().trim();
+            // clear the input on the screen
+            $("input").val("");
 
-        // If the search term is new, add it to the array.
-        if(this.animalArray.indexOf(input) === -1) {
-            this.animalArray.push(input);
-            this.buttonCreation(input)
+            // If the search term is new, add it to the array.
+            if(this.animalArray.indexOf(input) === -1) {
+                this.animalArray.push(input);
+                this.buttonCreation(input)
+            }
+
+            // Update the searchterm with the input and do an image pull.
+            // Should not hinder user from intended search of image.
+            this.searchterm = input;
+            this.imagepull();
         }
-
-        // Update the searchterm with the input and do an image pull.
-        // Should not hinder user from intended search of image.
-        this.searchterm = input;
-        this.imagepull();
+        else{
+            $("input").val("");
+        }
     },
 
     // Functions
@@ -37,16 +43,25 @@ var app = {
         for(var i = 0; i < array.length ; i++) {
             this.buttonCreation(array[i])
         }
-
-        this.buttonListener();
     },
 
     buttonCreation : function(value) {
         var buttondiv = $("<button>");
         buttondiv.addClass("btn btn-dark mx-2 my-1 button");
-        buttondiv.text(value);
+
+        var textdiv = $("<div>");
+        textdiv.addClass("text")
+        textdiv.text(value);
+
+        var closediv = $("<div>");
+        closediv.text("x");
+        closediv.addClass("close");
+
+        buttondiv.append(textdiv, closediv)
 
         $("#buttoncontainer").prepend(buttondiv);
+
+        this.buttonListener();
     },
 
     imagepull : function() {
@@ -65,7 +80,14 @@ var app = {
         imageContainer.append(imageHeader, randompulldiv);
         $("#massivecontainer").prepend(imageContainer);
 
-        this.jaxNumber = 0;
+        // use this to check the ajax and see its current state
+        var jaxNumber = 0;
+
+        // disable the use of the listeners;
+        $(".button").attr("disabled", true);
+        $("#add").off("click");
+        $("#input").off("keypress");
+
 
         for(var random = 0; random < 10; random++) {
             $.ajax({
@@ -74,6 +96,7 @@ var app = {
                 method: "GET"
             }).then( function(response) {
                 var data = response.data;
+
                 // container div
                 var containerdiv = $("<div>");
                 containerdiv.addClass("card border-secondary cardcontainer");
@@ -88,27 +111,44 @@ var app = {
 
                 // card body
                 var cardbody = $("<div>");
-                cardbody.addClass("card-body")
+                cardbody.addClass("card-body row")
 
                 // card details, currently has title.
                 var title = $("<h5>");
-                title.addClass("card-title");
+                title.addClass("card-title col-9 m-0");
                 title.html(data.title);
 
-                // Add a rating of the title
 
-                // Add a heart for the image card
+                var buttondiv = $("<div>");
+                buttondiv.addClass("col-3 m-0 p-0")
 
-                cardbody.append(title);
+                // Add a heart for like
+                var hearti = $("<i>");
+                hearti.addClass("btn btn-outline-danger btn-sm btn-block fas fa-heart m-0 pz-0")
+
+                // Add a copy for copying the url
+                var copyi = $("<i>");
+                copyi.addClass("btn btn-outline-primary btn-sm btn-block fas fa-link m-0 pz-0")
+                copyi.attr("link", data.fixed_height_small_url)
+
+                // Add a link to go to Giphy page (navigate, go to)
+
+                buttondiv.append(hearti, copyi);
+                cardbody.append(title, buttondiv);
                 containerdiv.append(img, cardbody);
 
                 $(randompulldiv).append(containerdiv);
 
 
-                app.jaxNumber += 1;
-                if(app.jaxNumber === 10) {
+                jaxNumber += 1;
+                if(jaxNumber === 10) {
 
                     app.gifListener();
+                    // turn back on the listeners
+                    $(".button").attr("disabled", false);
+                    app.inputListener();
+                    app.favoriteListener();
+                    app.copyListener();
                 }
 
             });
@@ -119,16 +159,19 @@ var app = {
     buttonListener : function() {
         $(".button").off("click");
         app.buttonListenerVariable = $(".button").on("click", function() {
-
-            app.searchterm = $(this).text();
+            console.log($(this).children(".text").text())
+            app.searchterm = $(this).children(".text").text();
             app.imagepull();
         })
     },
 
+    // Listens to the input from the text field
     inputListener : function() {
-        $("#add").click(app.inputFunction)
+        // Triggers the inputFunction on the add button
+        $("#add").on("click", app.inputFunction)
 
-        $("#input").keypress(newInput)
+        // Pressing enter or alt will tigger the inputFunction
+        $("#input").on("keypress", newInput)
 
         function newInput(key) {
             if(key.keyCode === 13 && !(key.altKey)) {
@@ -150,7 +193,36 @@ var app = {
                 $(this).attr("state", "still");
             }
         });
-    }
+    },
+
+    favoriteListener : function() {
+        $(".fa-heart").off("click");
+        app.favoriteListenerVariable = $(".fa-heart").on("click", function () {
+            var favorite = $(this).parent().parent().parent().clone();
+            favorite.addClass("mt-2");
+            $("#sidebar").append(favorite);
+        });
+
+    },
+
+    copyListener : function() {
+        $(".fa-link").off("click");
+        app.copyListenerVariable = $(".fa-link").on("click", function() {
+            // Create a div that will instantaneously be created to copy the value
+            var copydiv = $("<div>");
+            // Add the text of the attribute link
+            copydiv.text($(this).attr("link"));
+            copydiv.attr("id", "uniquecopy");
+            // append the child to select and copy the text
+            $("#sidebar").append(copydiv);
+            $("#uniquecopy").addClass("test")
+            $("#uniquecopy").select();
+            
+            // document.execCommand("copy");
+            // remove the copy so there is no trace
+            // copydiv.remove();
+        })
+    },
 }
 
 app.initialize();
