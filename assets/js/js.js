cb9c6e7c7b0c252e4cb60aadbbf057ab7a1832e3
Calvin Shin <calvin.shin@gmail.com>
2019-04-18 23:57:56 -0700

    Created local storage to handle favorites and saved terms. Improved responsive design. Last step is to design a pause button for giphy for users to understand that all images are paused

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,30 +1,77 @@
+// Things I want to do
+    // fix viewport issues 
+    // Pause button on the image transparently. Switching quickly to play and then disappears.
+    // Clicking again adds the pause button
+
 // javascript for the image browsing app
 
 var app = {
     // ajax variables that are important
     initialize : function() {
-        this.searchterm = "cats";
-        this.animalArray = ["cats", "dogs", "rabbits", "mongooses", "koalas", "manatees", "narwhals", "reindeer"];
+        this.searchterm = "cat";
+        this.animalArray = JSON.parse(localStorage.getItem("searchArray"));
+        if(!Array.isArray(this.animalArray)) {
+            this.animalArray = ["cat", "dog", "rabbit", "mongoose", "koala", "manatee", "narwhal", "reindeer"];
+        };
         this.numberResults = 0;
         this.jaxNumber = 0;
+        this.favoriteArray = JSON.parse(localStorage.getItem("favoriteArray"));
+        if(!Array.isArray(this.favoriteArray)) {
+            this.favoriteArray = [];
+        };
+
+        this.localStorageFavoriteCreation();
 
         // Starts the button creation
         this.buttonArray(this.animalArray);
-        this.inputListener();
+        this.allListeners();
     },
 
+    localStorageFavoriteCreation : function() {
+        // Starts the favorite list with previously created items
+        var randompulldiv = $("<div>");
+        randompulldiv.addClass("randompulldiv")
+        $("#massivecontainer").append(randompulldiv);
+
+        // Creates a card in the regular massivecontainer div
+        for(var i = 0; i < this.favoriteArray.length; i++) {
+            this.cardCreator(this.favoriteArray[i]);
+        }
+
+        // use the favoritecreator for each card in this randompulldiv
+        // this array is not a jquery item, so cannot use jquery on it; must use vanilla
+        var testarray = $(".cardcontainer");
+
+        for(var j = 0; j < testarray.length; j++) {
+            testarray[j].classList.add("mt-2");
+            $("#sidebarimage").prepend(testarray[j]);
+
+            // Change the heart to a X
+            var favoriteButton = $(".cardcontainer").find(".fa-heart");
+            favoriteButton.removeClass("fa-heart btn-outline-danger");
+            favoriteButton.addClass("fa-times-circle btn-outline-dark");
+        }
+
+        // remove the randompulldiv
+        randompulldiv.remove();
+    },
+    
     // triggered on the input being pressed
     inputFunction : function() {
         if($("input").val().trim() != "") {
             // Update the input as a value; set it as a local variable
-            var input = $("input").val().trim();
+            var input = $("input").val().trim().toLowerCase();
             // clear the input on the screen
             $("input").val("");
 
             // If the search term is new, add it to the array.
             if(this.animalArray.indexOf(input) === -1) {
+                // push this to localstorage; due to the prepend of the buttons, push is usable. If items were appended, an unshift would be used instead
                 this.animalArray.push(input);
-                this.buttonCreation(input)
+                localStorage.setItem("searchArray", JSON.stringify(this.animalArray));
+
+                // Create a new button
+                this.buttonCreation(input);
             }
 
             // Update the searchterm with the input and do an image pull.
@@ -40,8 +87,10 @@ var app = {
     // Functions
     buttonArray : function(array) {
         // Create buttons for the values in the searchArray.
+        localStorage.setItem("searchArray", JSON.stringify(this.animalArray));
+
         for(var i = 0; i < array.length ; i++) {
-            this.buttonCreation(array[i])
+            this.buttonCreation(array[i]);
         }
     },
 
@@ -57,7 +106,7 @@ var app = {
         closediv.text("x");
         closediv.addClass("close");
 
-        buttondiv.append(textdiv, closediv)
+        buttondiv.append(textdiv, closediv);
 
         $("#buttoncontainer").prepend(buttondiv);
 
@@ -66,15 +115,15 @@ var app = {
 
     imagepull : function() {
         // Wrap all 10 into a container
-        imageContainer = $("<div>");
+        var imageContainer = $("<div>");
         imageContainer.addClass("container");
 
         // Add a header to this container
-        imageHeader = $("<h2>");
+        var imageHeader = $("<h2>");
         imageHeader.text(this.searchterm);
 
         // Create a new div that houses all the images and is a randompulldiv class, no longer id
-        randompulldiv = $("<div>");
+        var randompulldiv = $("<div>");
         randompulldiv.addClass("card-columns card-spacing randompulldiv");
 
         imageContainer.append(imageHeader, randompulldiv);
@@ -83,7 +132,7 @@ var app = {
         // use this to check the ajax and see its current state
         var jaxNumber = 0;
 
-        // disable the use of the listeners;
+        // disable the use of the listeners while the function is running;
         $(".button").attr("disabled", true);
         $("#add").off("click");
         $("#input").off("keypress");
@@ -128,6 +177,7 @@ var app = {
         img.attr("state", "still");
         img.attr("animated", data.fixed_height_small_url);
         img.attr("still", data.fixed_height_small_still_url);
+        img.attr("alt", data.title);
 
         // card body
         var cardbody = $("<div>");
@@ -140,16 +190,16 @@ var app = {
 
 
         var buttondiv = $("<div>");
-        buttondiv.addClass("col-3 m-0 p-0")
+        buttondiv.addClass("col-3 m-0 p-0");
 
         // Add a heart for like
         var hearti = $("<i>");
-        hearti.addClass("btn btn-outline-danger btn-sm btn-block fas fa-heart m-0 pz-0")
+        hearti.addClass("btn btn-outline-danger btn-sm btn-block fas fa-heart m-0 pz-0");
 
         // Add a copy for copying the url
         var copyi = $("<i>");
-        copyi.addClass("btn btn-outline-primary btn-sm btn-block fas fa-link m-0 pz-0")
-        copyi.attr("link", data.fixed_height_small_url)
+        copyi.addClass("btn btn-outline-primary btn-sm btn-block fas fa-link m-0 pz-0");
+        copyi.attr("link", data.fixed_height_small_url);
 
         // Add a link to go to Giphy page (navigate, go to)
 
@@ -157,7 +207,46 @@ var app = {
         cardbody.append(title, buttondiv);
         containerdiv.append(img, cardbody);
 
-        $(randompulldiv).append(containerdiv);
+        $(".randompulldiv").append(containerdiv);
+    },
+
+    // Given some card, create a copy of the card into favorites. Need this separate to recreate favorites on reload.
+    favoriteCreator : function(card) {
+        // Add a new class for the container to have a margin top of 2
+        card.addClass("mt-2");
+        // prepend the image to the favorite
+        $("#sidebarimage").prepend(card);
+
+        // Change the heart to a X on the clone
+        var favoriteButton = card.find(".fa-heart");
+        favoriteButton.removeClass("fa-heart btn-outline-danger");
+        favoriteButton.addClass("fa-times-circle btn-outline-dark");
+
+        // Change the function of the X to a new listener; closeFavoriteListener.
+        app.allListeners();
+    },
+
+    // an object that creates an object for localStorage. Required for both adding and removing favorites.
+    favoriteObjectCreator : function(favorite, isAdd) {
+        // Create a starter to pull all the content for the localStorage object
+        var favoriteArrayStarter = favorite.children(".image")[0];
+
+        // Create an object to push into localStorage
+        var favoriteObject = {
+            fixed_height_small_still_url : favoriteArrayStarter.getAttribute("still"),
+            fixed_height_small_url : favoriteArrayStarter.getAttribute("animated"),
+            title : favoriteArrayStarter.alt,
+        };
+
+        if(isAdd === 1) {
+            // push the object into the favoriteArray
+            this.favoriteArray.push(favoriteObject);
+            localStorage.setItem("favoriteArray", JSON.stringify(app.favoriteArray));
+        }
+        else {
+            this.favoriteArray.splice(this.favoriteArray.indexOf(favoriteObject), 1);
+            localStorage.setItem("favoriteArray", JSON.stringify(app.favoriteArray));
+        }
     },
 
     allListeners : function() {
@@ -168,25 +257,42 @@ var app = {
         this.favoriteListener();
         this.closeFavoriteListener();
         this.copyListener();
+        this.closeButtonListener();
     },
 
     buttonListener : function() {
         $(".button").off("click");
-        app.buttonListenerVariable = $(".button").on("click", function() {
-            console.log($(this).children(".text").text())
-            app.searchterm = $(this).children(".text").text();
+        // now doesn't trigger on the entire button, just on the text.
+        app.buttonListenerVariable = $(".button").on("click", ":not(.close)", function() {
+            // console.log("test");
+            app.searchterm = $(this).text();
             app.imagepull();
             $("#sidebarheader").select();
         })
     },
 
+    closeButtonListener : function() {
+        $(".close").off("click");
+        app.closeButtonVariable = $(".close").on("click", function() {
+            // remove the item from the array app.animalArray
+            var removedItem = $(this).parent().children(".text").text();
+            app.animalArray.splice(app.animalArray.indexOf(removedItem), 1);
+
+            // Update the localstorage array
+            localStorage.setItem("searchArray", JSON.stringify(app.animalArray));
+
+            // remove the button
+            $(this).parent().remove();
+        });
+    },
+
     // Listens to the input from the text field
     inputListener : function() {
         // Triggers the inputFunction on the add button
-        $("#add").on("click", app.inputFunction)
+        $("#add").on("click", app.inputFunction);
 
         // Pressing enter or alt will tigger the inputFunction
-        $("#input").on("keypress", newInput)
+        $("#input").on("keypress", newInput);
 
         function newInput(key) {
             if(key.keyCode === 13 && !(key.altKey)) {
@@ -200,11 +306,11 @@ var app = {
         app.gifListenerVariable = $(".image").on("click", function() {
             var state = $(this).attr("state");
             if(state === "still") {
-                $(this).attr("src", $(this).attr("animated"))
+                $(this).attr("src", $(this).attr("animated"));
                 $(this).attr("state", "animated");
             }
             else {
-                $(this).attr("src", $(this).attr("still"))
+                $(this).attr("src", $(this).attr("still"));
                 $(this).attr("state", "still");
             }
         });
@@ -216,27 +322,22 @@ var app = {
         app.favoriteListenerVariable = $(".fa-heart").on("click", function () {
             // Create a copy of the entire div
             var favorite = $(this).parent().parent().parent().clone();
-            // Add a new class for the container to have a margin top of 2
-            favorite.addClass("mt-2");
-            // prepend the image to the favorite
-            $("#sidebarimage").prepend(favorite);
+            var isAdd = 1;
 
-            // Change the heart to a X
-            var favoriteButton = favorite.find(".fa-heart");
-            favoriteButton.removeClass("fa-heart btn-outline-danger");
-            favoriteButton.addClass("fa-times-circle btn-outline-dark")
+            app.favoriteObjectCreator(favorite, isAdd);
 
-            console.log(favorite);
-
-            // Change the function of the X to a new listener; closeFavoriteListener.
-
-            app.allListeners();
+            app.favoriteCreator(favorite);
         });
     },
 
     closeFavoriteListener : function() {
         $(".fa-times-circle").off("click");
         app.closeFavoritesListenerVariable = $(".fa-times-circle").on("click", function () {
+            var favorite = $(this).parent().parent().parent().clone();
+            var isAdd = 0;
+
+            app.favoriteObjectCreator(favorite, isAdd);
+
             $(this).parent().parent().parent().remove();
             app.allListeners();
         });
@@ -253,7 +354,7 @@ var app = {
             copydiv.attr("id", "uniquecopy");
             // append the child to select and copy the text
             $("#sidebar").append(copydiv);
-            $("#uniquecopy").addClass("test")
+            $("#uniquecopy").addClass("test");
             $("#uniquecopy").select();
             
             document.execCommand("copy");
